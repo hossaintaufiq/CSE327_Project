98585cf5cd565b6fa636c0b94a8a2dc7d78f51e4
Hossain Ahmmed <hossainahmmedtaufiq22@gmail.com>
2025-11-03 23:47:54 +0600

    added backend

---
+++ CRM/backend/server.js
@@ -0,0 +1,18 @@
+// server.js
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello from Node.js backend!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
