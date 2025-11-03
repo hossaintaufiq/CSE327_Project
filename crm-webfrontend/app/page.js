12d24276e633f15af2828fbd47a1af4a7e021ca3
Hossain Ahmmed <hossainahmmedtaufiq22@gmail.com>
2025-11-03 23:31:46 +0600

    settingup the web frontend

---
+++ crm-webfrontend/app/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <h1 className="text-4xl font-bold text-zinc-900 dark:text-zinc-100">
+        Welcome to CRM Web Frontend!
+      </h1>
+    </div>
+  );
+}
