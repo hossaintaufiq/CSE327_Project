7247238a8c71a7448975984914a21ed49ad2dd2a
Hossain Ahmmed <hossainahmmedtaufiq22@gmail.com>
2025-11-09 09:12:15 +0600

    added login system and signup

--- CRM/Client-Web/app/layout.js
+++ CRM/Client-Web/app/layout.js
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import "./globals.css";
 
 export default function RootLayout({ children }) {
@@ -5,13 +6,15 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className="bg-zinc-50 dark:bg-black text-zinc-900 dark:text-zinc-100">
         <header className="w-full py-4 px-6 flex justify-between items-center border-b border-zinc-200 dark:border-zinc-800">
-          <h1 className="font-bold text-lg">CRM Prime</h1>
+         <Link href="/" className="font-bold text-lg">CRM Prime</Link>
 
           <nav className="flex gap-4 text-sm">
-            <a href="/" className="hover:text-indigo-600">Home</a>
-            <a href="/dashboard" className="hover:text-indigo-600 font-medium">
+            <Link href="/" className="hover:text-indigo-600">Home</Link>
+            <Link href="/dashboard" className="hover:text-indigo-600 font-medium">
               Dashboard
-            </a>
+            </Link>
+            <Link href="/auth/login" className="hover:text-indigo-600">Login</Link>
+            <Link href="/auth/signup" className="hover:text-indigo-600">Sign Up</Link>
           </nav>
         </header>
 
