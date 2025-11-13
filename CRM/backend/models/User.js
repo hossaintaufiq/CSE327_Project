61da99293a9764766216bf02fc97abb30dc1a551
Hossain Ahmmed <hossainahmmedtaufiq22@gmail.com>
2025-11-13 18:46:45 +0600

    reinitializing the backend

--- CRM/backend/models/User.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const userSchema = new mongoose.Schema({
-	name: { type: String, trim: true },
-	email: { type: String, unique: true, lowercase: true, trim: true },
-	role: { type: String, enum: ['vendor', 'customer', 'admin'], default: 'customer' }
-}, { timestamps: true });
-
-module.exports = mongoose.model('User', userSchema);
--- CRM/backend/routes/subscriptionRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { createSubscription } from "../controllers/subscriptionController.js";
-import { verifyToken } from "../middlewares/authMiddleware.js";
-
-const router = express.Router();
-
-router.post("/create", verifyToken, createSubscription);
-
-export default router;
