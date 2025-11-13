19b9712213c7cc16cfd6c2a1db265fe4a8045f08
Naafuew <naafuewh.studies@gmail.com>
2025-11-13 17:44:43 +0600

    Implemented Color.kt

--- CRM/Client-app/app/src/main/java/com/example/clientapp/ui/theme/Color.kt
+++ CRM/Client-app/app/src/main/java/com/example/clientapp/ui/theme/Color.kt
@@ -1,1 +1,30 @@
-// TODO: Implemnt
+package com.example.clientapp.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val IndigoPrimary = Color(0xFF4F46E5)
+val IndigoLight = Color(0xFF818CF8)
+val IndigoDark = Color(0xFF312E81)
+
+// For backgrounds
+val BackgroundLight = Color(0xFFF9FAFB)
+val BackgroundDark = Color(0xFF0B0B0C)
+
+// For Card backgrounds
+val SurfaceLight = Color(0xFFFFFFFF)
+val SurfaceDark = Color(0xFF1C1C1E)
+
+// For text
+val TextPrimaryLight = Color(0xFF111827)
+val TextPrimaryDark = Color(0xFFF9FAFB)
+val TextSecondaryLight = Color(0xFF6B7280)
+val TextSecondaryDark = Color(0xFF9CA3AF)
+
+val Success = Color(0xFF22C55E)
+val Warning = Color(0xFFFACC15)
+val Error = Color(0xFFEF4444)
+val Info = Color(0xFF3B82F6)
+
+val DividerLight = Color(0xFFE5E7EB)
+val DividerDark = Color(0xFF2F2F30)
+
