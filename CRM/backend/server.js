451ccc957031c010e7eff4ee661f3c76730dba39
Hossain Ahmmed <hossainahmmedtaufiq22@gmail.com>
2025-11-13 19:18:59 +0600

    database connected successfully

--- CRM/backend/server.js
+++ CRM/backend/server.js
@@ -1,9 +1,10 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-// import connectDB from "./config/db.js"; // later
+import connectDB from "./config/db.js";
 
 dotenv.config();
+connectDB();
 
 const app = express();
 
@@ -11,10 +12,8 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("✅ Backend running successfully!");
+  res.send("✅ Backend and MongoDB connected successfully!");
 });
-
-// await connectDB(); // Uncomment later when DB ready
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
