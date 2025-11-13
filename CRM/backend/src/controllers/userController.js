8c28d5bc9d474527b9c74d8d6273dfa5e8830204
Hossain Ahmmed <hossainahmmedtaufiq22@gmail.com>
2025-11-13 18:59:10 +0600

    adding database

---
+++ CRM/backend/src/controllers/userController.js
@@ -0,0 +1,21 @@
+// src/controllers/userController.js
+import User from "../models/userModel.js";
+
+export const getUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ message: "Server Error" });
+  }
+};
+
+export const createUser = async (req, res) => {
+  try {
+    const { name, email } = req.body;
+    const user = await User.create({ name, email });
+    res.status(201).json(user);
+  } catch (error) {
+    res.status(400).json({ message: "Failed to create user" });
+  }
+};
---
+++ CRM/backend/src/models/userModel.js
@@ -0,0 +1,10 @@
+// src/models/userModel.js
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+});
+
+const User = mongoose.model("User", userSchema);
+export default User;
