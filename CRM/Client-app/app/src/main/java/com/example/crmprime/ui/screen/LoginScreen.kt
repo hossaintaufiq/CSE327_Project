f92f3aa7d058b7c2e4259a04a53f4c3498089ac2
hossaintaufiq <hossainahmmedtaufiq22@gmail.com>
2025-12-02 22:51:22 +0600

    app build done . have some issues in the backend

--- CRM/Client-app/app/src/main/java/com/example/crmprime/ui/screen/LoginScreen.kt
+++ CRM/Client-app/app/src/main/java/com/example/crmprime/ui/screen/LoginScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
@@ -155,4 +156,3 @@ fun LoginScreen(
         )
     }
 }
-
